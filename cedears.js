2c917b92325750b6ebdf05e70606d620b0e8a471
cqueirot-eng <cqueirot@gmail.com>
2026-07-22 00:30:53 -0300

    Agregar base inicial de ratios CEDEAR

---
+++ cedears.js
@@ -0,0 +1,41 @@
+window.CEDEAR_DATA = {
+  AAPL: {
+    nombre: "APPLE INC",
+    ratioCedears: 20,
+    ratioSubyacente: 1,
+    ratioTexto: "20:1",
+    mercado: "NASDAQ"
+  },
+
+  SPY: {
+    nombre: "SPDR S&P 500 ETF TRUST",
+    ratioCedears: 60,
+    ratioSubyacente: 1,
+    ratioTexto: "60:1",
+    mercado: "NYSE ARCA"
+  },
+
+  QQQ: {
+    nombre: "INVESCO QQQ TRUST",
+    ratioCedears: 20,
+    ratioSubyacente: 1,
+    ratioTexto: "20:1",
+    mercado: "NASDAQ"
+  },
+
+  GLD: {
+    nombre: "SPDR GOLD SHARES",
+    ratioCedears: 50,
+    ratioSubyacente: 1,
+    ratioTexto: "50:1",
+    mercado: "NYSE ARCA"
+  },
+
+  IBIT: {
+    nombre: "ISHARES BITCOIN TRUST ETF",
+    ratioCedears: 10,
+    ratioSubyacente: 1,
+    ratioTexto: "10:1",
+    mercado: "NASDAQ"
+  }
+};
