78b966267ed581b237baf4622b330ed1304e2867
cqueirot-eng <cqueirot@gmail.com>
2026-07-22 21:15:44 -0300

    Agregar ratios CEDEAR faltantes

--- cedears.js
+++ cedears.js
@@ -1,4 +1,5 @@
 window.CEDEAR_DATA = {
+
   AAPL: {
     nombre: "APPLE INC",
     ratioCedears: 20,
@@ -37,5 +38,102 @@ window.CEDEAR_DATA = {
     ratioSubyacente: 1,
     ratioTexto: "10:1",
     mercado: "NASDAQ"
+  },
+
+  "BRK.B": {
+    nombre: "BERKSHIRE HATHAWAY INC CL B",
+    ratioCedears: 22,
+    ratioSubyacente: 1,
+    ratioTexto: "22:1",
+    mercado: "NYSE"
+  },
+
+  DE: {
+    nombre: "DEERE & COMPANY",
+    ratioCedears: 40,
+    ratioSubyacente: 1,
+    ratioTexto: "40:1",
+    mercado: "NYSE"
+  },
+
+  ECL: {
+    nombre: "ECOLAB INC",
+    ratioCedears: 56,
+    ratioSubyacente: 1,
+    ratioTexto: "56:1",
+    mercado: "NYSE"
+  },
+
+  FSLR: {
+    nombre: "FIRST SOLAR INC",
+    ratioCedears: 18,
+    ratioSubyacente: 1,
+    ratioTexto: "18:1",
+    mercado: "NASDAQ"
+  },
+
+  LLY: {
+    nombre: "ELI LILLY",
+    ratioCedears: 56,
+    ratioSubyacente: 1,
+    ratioTexto: "56:1",
+    mercado: "NYSE"
+  },
+
+  MCD: {
+    nombre: "MCDONALD'S",
+    ratioCedears: 24,
+    ratioSubyacente: 1,
+    ratioTexto: "24:1",
+    mercado: "NYSE"
+  },
+
+  META: {
+    nombre: "META PLATFORMS",
+    ratioCedears: 24,
+    ratioSubyacente: 1,
+    ratioTexto: "24:1",
+    mercado: "NASDAQ"
+  },
+
+  NEE: {
+    nombre: "NEXTERA ENERGY",
+    ratioCedears: 19,
+    ratioSubyacente: 1,
+    ratioTexto: "19:1",
+    mercado: "NYSE"
+  },
+
+  NTES: {
+    nombre: "NETEASE",
+    ratioCedears: 14,
+    ratioSubyacente: 1,
+    ratioTexto: "14:1",
+    mercado: "NASDAQ"
+  },
+
+  O: {
+    nombre: "REALTY INCOME",
+    ratioCedears: 13,
+    ratioSubyacente: 1,
+    ratioTexto: "13:1",
+    mercado: "NYSE"
+  },
+
+  TSLA: {
+    nombre: "TESLA",
+    ratioCedears: 15,
+    ratioSubyacente: 1,
+    ratioTexto: "15:1",
+    mercado: "NASDAQ"
+  },
+
+  WMT: {
+    nombre: "WALMART",
+    ratioCedears: 18,
+    ratioSubyacente: 1,
+    ratioTexto: "18:1",
+    mercado: "NYSE"
   }
+
 };
